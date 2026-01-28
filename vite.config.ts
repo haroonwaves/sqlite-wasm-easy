4d0c35296669d736768dfe794b4aa9cb5160d1ca
Haroon <haroonusman00@gmail.com>
2026-01-28 18:59:40 +0530

    👷 chore: configure vite to use `vite-plugin-dts` for generating TypeScript declaration files.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import { fileURLToPath } from 'url';
+import dts from 'vite-plugin-dts';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
 	root: '.', // Root directory for dev server
+	plugins: [dts({ rollupTypes: true })],
 	build: {
 		lib: {
 			entry: {
